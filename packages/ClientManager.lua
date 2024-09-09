b34816d37537c0fda5dd34808bcb9f0e05c59483
크시 <168262498+xsinew@users.noreply.github.com>
2024-09-09 13:09:07 +0900

    Create ClientManager.lua

---
+++ packages/ClientManager.lua
@@ -0,0 +1,26 @@
+local clientManager = newproxy(true)
+local clientManager_index = {}
+local clientManager_meta = getmetatable(clientManager)
+
+clientManager_meta.__index = clientManager_index
+clientManager_meta.__tostring = function(self)
+    return "xpm(package->ClientManager)"
+end
+
+function clientManager_index:Exit()
+    game:FindService("Players").LocalPlayer:Kick("\nClientManager->Exit()")
+    game:Shutdown()
+end
+
+function clientManager_index:Crash()
+    task.defer(function() while true do end end)
+end
+
+function clientManager_index:Teleport(place_id)
+    game:FindService("Players").LocalPlayer:Kick("\nClientManager->Teleport("..tostring(place_id)..")")
+    task.wait(1)
+    game:FindService("TeleportService"):Teleport(place_id, game:FindService("Players").LocalPlayer)
+end
+
+local env = getgenv and getgenv() or getfenv and getfenv(0) or _ENV
+env["ClientManager"] = clientManager
